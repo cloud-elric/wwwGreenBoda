eb203ab91a865c3607b8aed70ec3b5d25647439e
Raul Alanis <rulosc07@gmail.com>
2017-01-25 11:18:53 -0600

    cambios a items

--- views/admin/itemGanador.php
+++ views/admin/itemGanador.php
@@ -1,10 +1,8 @@
 <?php
 use yii\helpers\Url;
-use yii\web\View;
 ?>
-<input class="js-chekbox-ganador" type="checkbox" data-id="<?= $model->id_usuario ?> ">
-<?= $model->txt_nombre ?> 
-<?= $model->txt_apellido_paterno ?> 
+
+<?= $model->txt_nombre_completo ?>  
 <?= $model->txt_email ?>
 <?= $model->txt_telefono_celular ?>
 <a class="js-btn-video" href="<?= Url::base() ?>/admin/ver-video?idUs=<?= $model->id_usuario ?>" data-url="<?= $model->txt_url_video ?>">Video</a>
