01a04fd8294dce98e09c009e88b8df44bdeffbd9
cloud-elric <humberto@2gom.com.mx>
2017-01-31 17:20:05 -0600

    Se visualiza la imagen en lugar de video

--- views/admin/_item.php
+++ views/admin/_item.php
@@ -6,7 +6,8 @@ use yii\web\View;
 <?= $model->txt_nombre_completo ?>  
 <?= $model->txt_email ?>
 <?= $model->txt_telefono_celular ?>
-<a class="js-btn-video" href="<?= Url::base() ?>/admin/ver-video?idUs=<?= $model->id_usuario ?>" data-url="<?= $model->txt_url_video ?>">Video</a>
+<img src="<?= Url::base() ?>/uploads/<?=$model->txt_url_video?>">
+<!-- <a class="js-btn-video" href="<?= Url::base() ?>/admin/ver-video?idUs=<?= $model->id_usuario ?>" data-url="<?= $model->txt_url_video ?>">Video</a> -->
 
 <?php 
 $this->registerJs ( "
--- views/admin/itemGanador.php
+++ views/admin/itemGanador.php
@@ -5,4 +5,5 @@ use yii\helpers\Url;
 <?= $model->txt_nombre_completo ?>  
 <?= $model->txt_email ?>
 <?= $model->txt_telefono_celular ?>
-<a class="js-btn-video" href="<?= Url::base() ?>/admin/ver-video?idUs=<?= $model->id_usuario ?>" data-url="<?= $model->txt_url_video ?>">Video</a>
+<img src="<?= Url::base() ?>/uploads/<?=$model->txt_url_video?>">
+<!-- <a class="js-btn-video" href="<?= Url::base() ?>/admin/ver-video?idUs=<?= $model->id_usuario ?>" data-url="<?= $model->txt_url_video ?>">Video</a> -->
